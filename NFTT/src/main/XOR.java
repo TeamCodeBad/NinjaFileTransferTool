157964d106cd04fba4f460fc7be998464299d23a
Josue Ruiz <josueruiz4747@gmail.com>
2016-11-28 17:17:25 -0800

    push

--- NFTT/src/main/XOR.java
+++ NFTT/src/main/XOR.java
@@ -63,12 +63,15 @@ public class XOR {
 		byte[] new_message = new byte[size];
 		if (state == 2) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 //			for (int i = 0; i < message.length; i++) {
 //				System.out.print((char)message[i]);
 //				if (i % 50 == 0) {
 //					System.out.print("\n");
 //				}
 //			}
+=======
+>>>>>>> parent of a6f0e08... Just checking something
 =======
 >>>>>>> parent of a6f0e08... Just checking something
 			new_message = new ArmorCoder().decodedManyChunks(message);
